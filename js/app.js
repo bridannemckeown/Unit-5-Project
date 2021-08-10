ca3bfbf2fd13e624b829b2e317dabcb77d6e2941
bridannemckeown <83795357+bridannemckeown@users.noreply.github.com>
2021-08-10 16:11:51 -0700

    Finalised searchFilter JS

--- js/app.js
+++ js/app.js
@@ -1,2 +1,34 @@
-const search = new Filter('search', 'data-caption');
+
+const searchFilter = () => {
+    const search = document.getElementById('search').value.toLowerCase();
+    const caption = document.getElementsByTagName('a');
+    for (i = 0; i < caption.length; i++) {
+        const text = caption[i].getAttribute('data-caption').toLowerCase();
+        if (!text.includes(search)) {
+            caption[i].style.display = "none";
+        } else {
+            caption[i].style.display = "block";
+        }
+    }
+}
+
+
+// const search = document.querySelector('#search');
+// const captions = document.querySelectorAll('[data-caption]');
+
+// const searchBar = event => {
+//     const searchTerm = event.target.value.toLowerCase();
+//     captions.forEach(captions => {
+//         const text = captions.textContent.toLowerCase();
+//         const imageLink = captions.parentElement;
+//         if(text.includes(searchTerm)) {
+//             imageLink.style.display = "block";
+//         } else {
+//             imageLink.style.display = "none";
+//         }
+//     });
+// }
+
+// search.addEventListener('keyup', searchBar);
+
 baguetteBox.run('.gallery');
