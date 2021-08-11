0eacd85b122811c6fdc0ac3342e80e5d6c073fc1
bridannemckeown <83795357+bridannemckeown@users.noreply.github.com>
2021-08-10 17:30:52 -0700

    refactoring original JS to work

--- js/app.js
+++ js/app.js
@@ -1,34 +1,33 @@
 
-const searchFilter = () => {
-    const search = document.getElementById('search').value.toLowerCase();
-    const caption = document.getElementsByTagName('a');
-    for (i = 0; i < caption.length; i++) {
-        const text = caption[i].getAttribute('data-caption').toLowerCase();
-        if (!text.includes(search)) {
-            caption[i].style.display = "none";
-        } else {
-            caption[i].style.display = "block";
-        }
-    }
-}
-
-
-// const search = document.querySelector('#search');
-// const captions = document.querySelectorAll('[data-caption]');
-
-// const searchBar = event => {
-//     const searchTerm = event.target.value.toLowerCase();
-//     captions.forEach(captions => {
-//         const text = captions.textContent.toLowerCase();
-//         const imageLink = captions.parentElement;
-//         if(text.includes(searchTerm)) {
-//             imageLink.style.display = "block";
+// const searchFilter = () => {
+//     const search = document.getElementById('search').value.toLowerCase();
+//     const caption = document.getElementsByTagName('a');
+//     for (i = 0; i < caption.length; i++) {
+//         const text = caption[i].getAttribute('data-caption').toLowerCase();
+//         if (!text.includes(search)) {
+//             caption[i].style.display = "none";
 //         } else {
-//             imageLink.style.display = "none";
+//             caption[i].style.display = "block";
 //         }
-//     });
+//     }
 // }
 
-// search.addEventListener('keyup', searchBar);
+
+const search = document.querySelector('#search');
+const captions = document.querySelectorAll('[data-caption]');
+
+const searchBar = event => {
+    const searchTerm = event.target.value.toLowerCase();
+    captions.forEach(captions => {
+        const text = captions.getAttribute('data-caption').toLowerCase();
+        if(text.includes(searchTerm)) {
+            captions.style.display = "block";
+        } else {
+            captions.style.display = "none";
+        }
+    });
+}
+
+search.addEventListener('keyup', searchBar);
 
 baguetteBox.run('.gallery');
